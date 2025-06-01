c5813e27cccc75ae8d10f0bb41a82ad896692759
fmarinoa <franco.edson.18014@gmail.com>
2025-06-01 16:15:26 -0500

    Implementar la interfaz Interaction en las clases Click y Type, mejorando la tipificación de los métodos

--- src/interactions/Click.ts
+++ src/interactions/Click.ts
@@ -1,15 +1,16 @@
+import { Interaction } from '../screenplay/Interaction';
 import { Actor } from '../actors/Actor';
 import { BrowseTheWeb } from '../abilities/BrowseTheWeb';
 
-export class Click {
-  static on(selector: string) {
-    return new Click(selector);
-  }
+export class Click implements Interaction {
+    static on(selector: string) {
+        return new Click(selector);
+    }
 
-  constructor(private selector: string) {}
+    constructor(private readonly selector: string) {}
 
-  async performAs(actor: Actor) {
-    const page = actor.abilityTo(BrowseTheWeb).getPage();
-    await page.click(this.selector);
-  }
+    async performAs(actor: Actor): Promise<void> {
+        const page = actor.abilityTo(BrowseTheWeb).getPage();
+        await page.click(this.selector);
+    }
 }
--- src/interactions/Type.ts
+++ src/interactions/Type.ts
@@ -1,16 +1,17 @@
+import { Interaction } from '../screenplay/Interaction';
 import { Actor } from '../actors/Actor';
 import { BrowseTheWeb } from '../abilities/BrowseTheWeb';
 
-export class Type {
+export class Type implements Interaction {
   static theText(value: string) {
     return {
       into: (selector: string) => new Type(value, selector),
     };
   }
 
-  constructor(private value: string, private selector: string) { }
+  constructor(private readonly value: string, private readonly selector: string) { }
 
-  async performAs(actor: Actor) {
+  async performAs(actor: Actor): Promise<void> {
     const page = actor.abilityTo(BrowseTheWeb).getPage();
     await page.fill(this.selector, this.value);
   }
