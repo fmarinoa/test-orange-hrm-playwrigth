34231a8d35e6e8e5a7304a78a2759009a2a80005
fmarinoa <franco.edson.18014@gmail.com>
2025-06-01 16:22:08 -0500

    Agregar la interfaz AnsweredBy y extenderla en la interfaz Question para mejorar la tipificación de métodos

--- src/screenplay/Question.ts
+++ src/screenplay/Question.ts
@@ -1,5 +1,6 @@
 import { Actor } from '../actors/Actor';
+import { AnsweredBy } from './AnsweredBy';
 
-export interface Question<T> {
-  answeredBy(actor: Actor): Promise<T> | T;
+export interface Question<T> extends AnsweredBy<T> {
+    answeredBy(actor: Actor): Promise<T> | T;
 }
