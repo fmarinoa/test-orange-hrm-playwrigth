741cbd01e09bad5582befa9f5ae25b9a5b3da759
fmarinoa <franco.edson.18014@gmail.com>
2025-06-01 16:10:37 -0500

    Implementar la interfaz Task en la clase Login y mejorar la tipificación de métodos

--- src/tasks/Login.ts
+++ src/tasks/Login.ts
@@ -1,16 +1,17 @@
+import { Task } from '../screenplay/Task';
 import { Actor } from '../actors/Actor';
 import { Type } from '../interactions/Type';
 import { Click } from '../interactions/Click';
 import { LoginPage } from '../pages/LoginPage';
 
-export class Login {
-  static withCredentials(username: string, password: string) {
+export class Login implements Task {
+  static withCredentials(username: string, password: string): Task {
     return new Login(username, password);
   }
 
-  constructor(private username: string, private password: string) {}
+  constructor(private readonly username: string, private readonly password: string) {}
 
-  async performAs(actor: Actor) {
+  async performAs(actor: Actor): Promise<void> {
     await actor.attemptsTo(
       Type.theText(this.username).into(LoginPage.usernameField),
       Type.theText(this.password).into(LoginPage.passwordField),
