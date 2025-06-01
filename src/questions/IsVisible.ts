0374c46916b1bacdf9fb0f9999e62f22ce52840f
fmarinoa <franco.edson.18014@gmail.com>
2025-06-01 16:11:37 -0500

    Implementar la interfaz Question en la clase IsVisible y mejorar la tipificación de métodos

--- src/questions/IsVisible.ts
+++ src/questions/IsVisible.ts
@@ -1,12 +1,13 @@
+import { Question } from '../screenplay/Question';
 import { Actor } from '../actors/Actor';
 import { BrowseTheWeb } from '../abilities/BrowseTheWeb';
 
-export class IsVisible {
-  static of(selector: string) {
+export class IsVisible implements Question<boolean> {
+  static of(selector: string): Question<boolean> {
     return new IsVisible(selector);
   }
 
-  constructor(private selector: string) {}
+  constructor(private readonly selector: string) {}
 
   async answeredBy(actor: Actor): Promise<boolean> {
     const page = actor.abilityTo(BrowseTheWeb).getPage();
