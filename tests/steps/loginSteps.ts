a10fb5dde9869c211557bf8522ea9ea390005393
fmarinoa <franco.edson.18014@gmail.com>
2025-06-22 22:18:24 -0500

    feat: agregar soporte asincrónico a los pasos de Cucumber en las pruebas de inicio de sesión

--- tests/steps/loginSteps.ts
+++ tests/steps/loginSteps.ts
@@ -1,21 +1,21 @@
-import {Given, When, Then } from '@cucumber/cucumber';
+import { Given, When, Then } from '@cucumber/cucumber';
 
-Given('I navigate to the login page', function () {
+Given('I navigate to the login page', async function () {
   // Code to navigate to the login page
 });
 
-When('I enter valid credentials', function () {
+When('I enter valid credentials',async function () {
   // Code to enter valid credentials
 });
 
-Then('I should be redirected to the dashboard', function () {
+Then('I should be redirected to the dashboard',async function () {
   // Code to verify redirection to the dashboard
 });
 
-When('I enter invalid credentials', function () {
+When('I enter invalid credentials',async function () {
   // Code to enter invalid credentials
 });
 
-Then('I should see an error message', function () {
+Then('I should see an error message',async function () {
   // Code to verify the error message is displayed
 });
