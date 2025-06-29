aae3c8e4027d15275022915c38cc79aeab1fc0f6
fmarinoa <franco.edson.18014@gmail.com>
2025-06-29 18:05:31 -0500

    refactor: simplificar la función createBrowser para usar la variable de entorno BROWSER

--- src/browser/browserFactory.ts
+++ src/browser/browserFactory.ts
@@ -6,9 +6,16 @@ const browserMap = {
     webkit: { launcher: webkit, name: "safari" }
 };
 
-export async function createBrowser(browserKey: string): Promise<{ browser: Browser, humanName: string, version: string }> {
-    const entry = browserMap[browserKey.toLowerCase().trim() as keyof typeof browserMap] || browserMap.chromium;
+export async function createBrowser(): Promise<{ browser: Browser, humanName: string }> {
+    let entry;
+
+    const browserKey = process.env.BROWSER;
+    if (!browserKey) {
+        entry = browserMap.chromium; // Default to Chromium if no BROWSER env variable is set
+    } else {
+        entry = browserMap[browserKey.toLowerCase().trim() as keyof typeof browserMap]
+    }
+
     const browser = await entry.launcher.launch({ headless: process.env.HEADLESS === "true" });
-    const version = browser.version();
-    return { browser, humanName: entry.name, version };
+    return { browser: browser, humanName: entry.name };
 }
