c5fcfda95a89261d5bececa0e8a656e18aaf1d2b
fmarinoa <franco.edson.18014@gmail.com>
2025-07-06 16:06:00 -0500

    fix: fix locoator

--- src/pages/loginPage.ts
+++ src/pages/loginPage.ts
@@ -9,7 +9,7 @@ export class LoginPage extends BasePage {
     private readonly usernameInput = 'input[name="username"]';
     private readonly passwordInput = 'input[name="password"]';
     private readonly loginButton = 'button[type="submit"]';
-    private readonly errorMessage = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]';
+    private readonly errorMessage = '.orangehrm-login-error div[role="alert"] div p';
 
     async typeUsername(username: string) {
         await this.type(this.usernameInput, username);
