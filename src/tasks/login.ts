339e95b918f313b48d4b01e3216d7eeeb000204e
fmarinoa <franco.edson.18014@gmail.com>
2025-07-06 12:19:39 -0500

    fix: Simplificar lógica sin fluent task

--- src/tasks/login.ts
+++ src/tasks/login.ts
@@ -1,11 +1,15 @@
+import { Page } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
-import { FluentTask } from './FluentTask';
 
-export const Login = new FluentTask<[string, string]>(
-    async (username, password, page) => {
-        const loginPage = new LoginPage(page);
-        await loginPage.typeUsername(username);
-        await loginPage.typePassword(password);
-        await loginPage.clickLoginButton();
+export class Login {
+    static with(username: string, password: string) {
+        return {
+            using: async (page: Page) => {
+                const loginPage = new LoginPage(page);
+                await loginPage.typeUsername(username);
+                await loginPage.typePassword(password);
+                await loginPage.clickLoginButton();
+            }
+        };
     }
-);
+}
